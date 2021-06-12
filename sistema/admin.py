b3c6f74a871c3bbeed6d3e6398262fe7275c4d15
Marcos Macedo <marcosmacedo30@hotmail.com>
2021-06-12 21:13:47 -0230

    Cambios

--- sistema/admin.py
+++ sistema/admin.py
@@ -5,8 +5,16 @@ from .models import *
 
 class VacunacionAdmin(admin.ModelAdmin):
     list_display = ('vacuna','persona', 'fecha', 'vacunatorio')
+    autocomplete_fields = ['persona', 'vacunatorio']
+
+class PersonaAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+
+class VacunatorioAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+
 
 admin.site.register(Vacuna)
 admin.site.register(Vacunacion, VacunacionAdmin)
-admin.site.register(Persona)
-admin.site.register(Vacunatorio)
+admin.site.register(Persona, PersonaAdmin)
+admin.site.register(Vacunatorio, VacunatorioAdmin)
